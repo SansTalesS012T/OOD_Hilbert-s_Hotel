3348cc2740cea03c648d26834fc380fbad8f6232
SansTalesS012T <Underverse678@gmail.com>
2025-09-26 21:30:51 +0700

    add hotel

---
+++ hotel.py
@@ -0,0 +1,14 @@
+import src.linked_list as lib
+
+class Hotel:
+    def __init__(self):
+        room_list = lib.DLL() # create doubly linked list named room_list
+
+    def add_room(self, num):
+        pass
+    
+    def del_room(self, num):
+        pass
+    
+    def search(self, num):
+        pass
