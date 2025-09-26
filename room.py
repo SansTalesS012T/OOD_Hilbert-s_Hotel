446eb07477fdf27c06831ded3956f817ed86ecad
SansTalesS012T <Underverse678@gmail.com>
2025-09-26 21:57:13 +0700

    room equal method

--- room.py
+++ room.py
@@ -6,8 +6,16 @@ class Room:
     def get_room_no(self): return self.__room_no
     
     def get_guest_no(self): return self.__guest_no
+    
+    def same_room_no(self, room_no): return self.__room_no == room_no 
+    
+    def same_guest_no(self, guest_no): return self.__guest_no == guest_no
 
 import src.linked_list as lib
 class RoomList(lib.DLL):
     def __init__(self):
         super.__init__()
+        
+    def del_room(self, num):
+        temp = self.get_head()
+        # while(temp != )
